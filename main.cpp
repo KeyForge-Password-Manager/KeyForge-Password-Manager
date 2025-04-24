968483551fc2d41c5b3a86c0c545c52a9944a312
LIS <islagunov@edu.hse.ru>
2025-04-24 18:58:24 +0300

    добавил работу с файлами

--- main.cpp
+++ main.cpp
@@ -1,7 +1,57 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 #include <vector>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
+
+
+#include <chrono>
+#include <thread>//	std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+
+
+
 using namespace std;
+
+class Color {
+public:
+	/*Color rr;	cout << rr.magenta("red") << "  " << rr.cyan("blue") <<"  "<<rr.grey("grey")<< endl;*/
+	string black(string str) {
+		str = "\033[30m" + str + "\033[0m";
+		return str;
+	}
+	string red(string str) {
+		str = "\033[31m" + str + "\033[0m";
+		return str;
+	}
+	string green(string str) {
+		str = "\033[32m" + str + "\033[0m";
+		return str;
+	}
+	string yellow(string str) {
+		str = "\x1b[33m" + str + "\x1b[0m";
+		return str;
+	}
+	string blue(string str) {
+		str = "\x1b[34m" + str + "\x1b[0m";
+		return str;
+	}
+	string magenta(string str) {
+		str = "\033[35m" + str + "\033[0m";
+		return str;
+	}
+	string cyan(string str) {
+		str = "\033[36m" + str + "\033[0m";
+		return str;
+	}
+	/*???*/string grey(string str) {
+		str = "\033[37m" + str + "\033[0m";
+		return str;
+	}
+};
+Color rr;
+
 
 string Vigenere(string pass, string key) {
 	string ans = "";
@@ -289,6 +339,144 @@ string Playfer_obr(string pass, string key) {
 	return ans;
 }
 
-int main(){
-  return 0;
+void norm_string(string& pass) {
+	for (int i = 0; i < pass.size(); i += 2) {
+		if (pass[i] == pass[i + 1]) {
+			pass.insert(i + 1, "X");
+		}
+	}
+	//if (pass.size() % 2 == 1)pass += 'X';
+}
+void norm_ans(string& s2){
+	for (int i = 0; i < s2.size(); i++) {
+		if (s2[i] == 'X') {
+			if (s2[i - 1] == s2[i + 1]) s2.erase(s2.begin() + i);
+		}
+	}
+}
+
+string read(string company) {
+	string ans="";
+	string line;
+	ifstream in("Storage.txt");
+	if (in.is_open())
+	{
+		while (getline(in, line))
+		{
+			bool similar = true;
+			for (int i = 0; i < company.size(); i++) {
+				if (company[i] != line[i])similar = false;
+			}
+			string p = "";
+			for (int i = line.find(' ')+1; i < line.size(); i++)p += line[i];
+			if (similar) ans += p+=" ";
+		}
+	}
+	in.close();
+	return ans;
+}
+void addf(string company, string pass) {
+	ofstream out;
+	out.open("Storage.txt", ios::app);
+	if (out.is_open())
+	{
+		string r = read(company);
+		if (r == "") {
+			out << company << " " << pass << endl;
+		}
+		else {
+			cout << "этот сайт(приложение) ранее уже был введён. Хотите заменить пароль(введите 1) или создать ещё один(введите 2)?"<<endl;
+			int inp; cin >> inp;
+			if(inp==2)out << company << " " << pass << endl;
+			if(inp==1) {
+				
+				ofstream out2;
+				out2.open("temp.txt");
+				string line2;
+				ifstream in2("Storage.txt");
+				if (in2.is_open())
+				{
+					while (getline(in2, line2))
+					{
+						bool similar = true;
+						for (int i = 0; i < company.size(); i++) {
+							if (company[i] != line2[i])similar = false;
+						}
+						if (!similar)
+							out2 << line2 << endl;
+						else {
+							cout << line2 << endl;
+							cout << "Если хотите оставить этот пароль, нажмите " << rr.blue("1") << endl <<
+								    "Если хотите удалить его, нажмите " << rr.red("2") << endl;
+							int inp; cin >> inp;
+							if (inp == 1)out2 << line2 << endl;
+						}
+					}
+				}
+				in2.close();
+				out2.close();
+
+				ofstream out3;
+				out3.open("Storage.txt");
+				string line3;
+				ifstream in3("temp.txt");
+				if (in3.is_open())
+				{
+					while (getline(in3, line3))
+					{
+						out3 <<line3 << endl;
+					}
+				}
+				out3 << company << " " << pass << endl;
+				in3.close();
+				out3.close();
+			}
+		}
+	}
+	out.close();
+}
+
+string encryption(string pass, string key){
+	string ans;
+	ans = Playfer(pass, key);
+	ans=Vigenere(ans, key);
+	return ans;
+}
+string decryption(string pass, string key){
+	string ans;
+	ans = Vigenere_obr(pass, key);
+	ans = Playfer_obr(ans, key);
+	return ans;
+}
+void read_all_file(string key) {
+	string line;
+	ifstream in("Storage.txt");
+	if (in.is_open())
+	{
+		while (getline(in, line))
+		{
+			string company = ""; string pass = "";
+			for (int i = 0; i < line.find(" "); i++)company += line[i];
+			for (int i = line.find(" ") + 1; i < line.size(); i++)pass += line[i];
+			cout << setw(15)<<left<< company << " "<< decryption(pass,key)<< endl;
+		}
+	}
+	in.close();
+}
+int count_str_in_file(string key) {
+	int n = 0;
+	string line;
+	ifstream in("Storage.txt");
+	if (in.is_open())
+	{
+		while (getline(in, line))
+		{
+			string company = ""; string pass = "";
+			for (int i = 0; i < line.find(" "); i++)company += line[i];
+			for (int i = line.find(" ") + 1; i < line.size(); i++)pass += line[i];
+			n++;
+		}
+	}
+	in.close();
+	return n;
 }
